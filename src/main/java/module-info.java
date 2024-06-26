9fb01cd058146edb13884fd060ad935d590451a5
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-26 13:40:05 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,7 @@ module com.tugalsan.api.sql.restbl {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.string;
-    requires com.tugalsan.api.validator;
+    
     requires com.tugalsan.api.sql.resultset;
     requires com.tugalsan.api.sql.select;
     exports com.tugalsan.api.sql.restbl.server;
